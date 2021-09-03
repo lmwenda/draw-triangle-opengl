51b6f664dbb2c868e214b201495826d3cbe39db4
number 1 dev <67679722+lmwenda@users.noreply.github.com>
2021-09-03 14:03:12 +0100

    Added OpenGL Triangle Application

---
+++ Learning OpenGL/src/Application.cpp
@@ -0,0 +1,49 @@
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Basic OpenGL Triangle Drawing", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Drawing a Triangle
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        glColor3f(0, 0, 0);
+
+        // Changing Background Colour
+        glClearColor(255, 255, 255, 0);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
